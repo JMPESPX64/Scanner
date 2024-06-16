ec12539603599b483833c7c01ff3a4c45efce1e0
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-06-16 16:19:11 +0200

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -4,6 +4,7 @@ domain="$1"
 proxy="socks5://127.0.0.1:1337"
 
 # Subdomain enumeration
+echo -e "Listing subdomains on $domain" | notify -bulk -silent
 python3 ~/tools/SubList3r/sublist3r.py -d $domain -o /root/results/$domain/subdomains/subdomains.txt
 chaos -d $domain -silent | grep "\.$domain$" | sed 's/*.//' | sort -u | anew /root/results/$domain/subdomains/subdomains.txt
 subfinder -d $domain -all -silent | anew /root/results/$domain/subdomains/subdomains.txt
@@ -11,6 +12,7 @@ assetfinder $domain --subs-only | grep "\.$domain$" | sed 's/*.//' | sort -u | a
 amass enum -passive -d $domain -noalts -norecursive | grep "\.$domain$" | sort -u | anew /root/results/$domain/subdomains/subdomains.txt
 
 # Subdomain bruteforce (Puredns)
+echo -e "Brute force with puredns on $domain" | notify -bulk -silent
 puredns -r /root/tools/massdns/lists/resolvers.txt /usr/share/seclists/Discovery/DNS/dns-Jhaddix.txt $domain --write puredns.txt
 cat puredns.txt | anew /root/results/$domain/subdomains/subdomains.txt
 rm puredns.txt
@@ -27,7 +29,9 @@ cat /root/results/$domain/httpx_output/alive_subdomains.txt | katana -jc -d 5 -e
 grep -v -f /root/tools/blacklist.txt /root/results/$domain/wayback_data/*.txt | tee -a /root/results/$domain/wayback_data/all-urls.txt
 
 # Php endpoints
+echo -e "Getting PHP endpoints on $domain" | notify -bulk -silent
 cat /root/results/$domain/wayback_data/all-urls.txt | grep "\.php$" | httpx -silent | anew /root/results/$domain/fuzzing/php-endpoints.txt
+echo -e "Total of PHP endpoints -> $(wc -l < /root/results/$domain/fuzzing/php-endpoints)"
 
 # Vulnerabilities
 
@@ -38,7 +42,9 @@ dalfox file /root/results/$domain/test_vulns/XSS.txt --waf-evasion --skip-mining
 echo -e "The dalfox scan have finished -> $(wc -l < /root/results/$domain/vulns/XSS.txt) results"
 
 # Secrets
-cat /root/results/$domain/wayback_data/all-urls.txt | grep "\.js$" | sort -u | httpx -silent | nuclei -t exposures -o /root/results/$domain/vuls/exposures.txt
+echo "Listing secrets with nuclei on $domain" | notify -bulk -silent
+cat /root/results/$domain/wayback_data/all-urls.txt | grep "\.js$" | sort -u | httpx -silent | nuclei -t exposures -o /root/results/$domain/vulns/exposures.txt
+echo -e "Nuclei (secrets) has finished -> $(wc -l < /root/results/$domain/vulns/exposures.txt)" | notify -bulk -silent
 
 #LFI (Paramspider)
 #cat /root/results/$domain/wayback_data/all-urls.txt | gf lfi | qsreplace 'FUZZ' | anew lfi-tmp.txt
