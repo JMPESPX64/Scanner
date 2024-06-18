2ec6b4ce529614f7c934f057556a113253afcfb8
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-06-18 17:40:50 +0200

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -1,96 +1,68 @@
-#!/bin/bash 
+#!/bin/bash
 
 domain="$1"
 proxy="socks5://127.0.0.1:1337"
 
-mkdir -p /root/results/$domain/{wayback_data,vulns,ports,aquatone,subdomains,httpx_info,fuzzing,technologies,test_vulns}
+alive_subdomains_path="/root/results/$domain/httpx_info/alive_subdomains.txt"
+wayback_data_path="/root/results/$domain/wayback_data"
+vulns_path="/root/results/$domain/vulns"
+param_spider_path="/root/Paramspider/domains"
+wordlist_dir="/root/tools/ElKraken/Tools/custom_wordlist.txt"
 
-# Subdomain enumeration
-echo -e "Listing subdomains on $domain" | notify -bulk -silent
-python3 ~/tools/SubList3r/sublist3r.py -d $domain -o /root/results/$domain/subdomains/subdomains.txt
-chaos -d $domain -silent | grep "\.$domain$" | sed 's/*.//' | sort -u | anew /root/results/$domain/subdomains/subdomains.txt
+echo "Listing subdomains on $domain" | notify -bulk -silent
+python3 /root/tools/SubList3r/sublist3r.py -d $domain -o /root/results/$domain/subdomains/subdomains.txt
 subfinder -d $domain -all -silent | anew /root/results/$domain/subdomains/subdomains.txt
-assetfinder $domain --subs-only | grep "\.$domain$" | sed 's/*.//' | sort -u | anew /root/results/$domain/subdomains/subdomains.txt
-amass enum -passive -d $domain -noalts -norecursive | grep "\.$domain$" | sort -u | anew /root/results/$domain/subdomains/subdomains.txt
-echo "$domain" | anew /root/results/$domain/subdomains/subdomains.txt
+chaos -d $domain -silent | grep "\.$domain$" | sed 's/*.//' | anew /root/results/$domain/subdomains/subdomains.txt
+assetfinder --subs-only $domain | grep "\.$domain$" | sed 's/*.//' | anew /root/results/$domain/subdomains/subdomains.txt
+amass -passive -d $domain -noalts -norecursive | anew /root/results/$domain/subdomains/subdomains.txt
+echo "Total passive subdomains found on $domain -> $(wc -l < /root/results/$domain/subdomains/subdomains.txt)" | notify -bulk -silent
 
-# Subdomain bruteforce (Puredns)
-echo -e "Brute force with puredns on $domain" | notify -bulk -silent
+# Bruteforce
+echo "Bruteforce with puredns on $domain" | notify -bulk -silent
 puredns bruteforce -r /root/tools/ElKraken/Tools/resolvers.txt /usr/share/seclists/Discovery/DNS/dns-Jhaddix.txt $domain --write puredns.txt
 cat puredns.txt | anew /root/results/$domain/subdomains/subdomains.txt
-grep -v -f /root/tools/blacklist.txt /root/results/$domain/subdomains/subdomains.txt | sponge /root/results/$domain/subdomains/subdomains.txt
+echo "Total subdomains after run puredns on $domain -> $(wc -l < /root/results/$domain/subdomains/subdomains.txt)" | notify -bulk -silent
 
 # Alive subdomains
 echo "Running httpx on $domain" | notify -bulk -silent
-httpx -l /root/results/$domain/subdomains/subdomains.txt -t 100 -silent | tee -a /root/results/$domain/httpx_info/alive_subdomains.txt
-echo "$(wc -l < /root/results/$domain/httpx_info/alive_subdomains.txt) Alive subdomains" | notify -bulk -silent
+httpx -l /root/results/$domain/subdomains/subdomains.txt -silent | tee -a /root/results/$domain/httpx_info/alive_subdomains.txt
+echo -e "Alive subdomains $(wc -l < $alive_subdomains_path)" | notify -bulk -silent
 
 # Wayback Data
-echo "Listing URLS with mutliple tools (Archive.org + KATANA)" | notify -bulk -silent
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | gau --threads 16 --subs --blacklist png,jpg,jpeg,gif,woff,woff2,ico,svg | tee -a /root/results/$domain/wayback_data/gau.txt
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | waybackurls | tee -a /root/results/$domain/wayback_data/waybackurls.txt
-waymore -i $domain -mode U -oU /root/results/$domain/wayback_data/waymore.txt
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | katana -jc -d 5 -ef png,jpg,jpeg,gif,woff,woff2,ico,svg | tee -a /root/results/$domain/wayback_data/katana.txt
-paramspider -l /root/results/$domain/httpx_info/alive_subdomains.txt
-grep -v -f /root/tools/blacklist.txt /root/results/$domain/wayback_data/*.txt | anew /root/results/$domain/wayback_data/all-urls.txt
-cat /root/tools/Paramspider/domains/*.txt | anew /root/results/$domain/wayback_data/all-urls.txt
+cat $alive_subdomains_path | gau --threads 16 --subs --blacklist jpg,png,woff,woff2,ico,svg,gif,jpeg | tee -a $wayback_data_path/gau.txt
+cat $alive_subdomains_path | waybackurls | tee -a $wayback_data_path/waybackurls.txt
+waymore -i $domain -mode U -oU $wayback_data_path/waymore.txt
+cat $alive_subdomains_path | katana -jc -d 5 -silent | tee -a $wayback_data_path/katana.txt
+paramspider -l $alive_subdomains_path
 
-# PHP endpoints
-echo -e "Getting PHP endpoints on $domain" | notify -bulk -silent
-cat /root/results/$domain/wayback_data/all-urls.txt | grep -E "\.php" | httpx -silent | anew /root/results/$domain/fuzzing/php-endpoints.txt
-echo -e "Total of PHP endpoints -> $(wc -l < /root/results/$domain/fuzzing/php-endpoints)" | notify -bulk -silent
+# Nuclei custom templates
 
 # XSS
-echo -e "Running Kxss on $domain" | notify -bulk -silent
-cat /root/results/$domain/wayback_data/all-urls.txt /root/tools/Paramspider/domains/*.txt | gf xss | uro | kxss | grep -v "\[\]" | tee -a /root/results/$domain/vulns/XSS.txt
-echo "Kxss has finished on $domain" | notify -bulk -silent
+echo "Running kxss on $domain" | notify -bulk -silent
+cat $wayback_data_path/*.txt | gf xss | uro | kxss | grep -v "\[\]" | tee -a $vulns_path/XSS.txt
+cat $param_spider_path/*.txt | kxss | grep -v "\[\]" | anew $vulns_path/XSS.txt
+echo "Kxss has finished -> $(cat $vulns_path/XSS.txt | grep '<' | wc -l) posible XSS" | notify -bulk -silent
+
+# XSS (Freq)
+echo "Running freq on $domain" | notify -bulk -silent
+cat $wayback_data_path/*.txt $param_spider_path/*.txt | grep "=" | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq
+echo "Freq has finished on $domain"
 
 # Secrets
-echo "Listing secrets with nuclei on $domain" | notify -bulk -silent
-cat /root/results/$domain/wayback_data/all-urls.txt | grep "\.js$" | httpx -silent | nuclei -t exposures -rl 40 -c 20 -H "X-Forwarded-For: 127.0.0.1" -o /root/results/$domain/vulns/exposures.txt
-echo -e "Nuclei (secrets) has finished -> $(wc -l < /root/results/$domain/vulns/exposures.txt)" | notify -bulk -silent
+echo -e "Runnig nuclei for list secrets on $domain" | notify -bulk -silent
+cat $wayback_data_path/*.txt | grep "\.js$" | httpx -silent | tee -a $wayback_data_path/js.txt
+nuclei -l $wayback_data_path/js.txt -rl 40 -c 20 -t exposures -o $vulns_path/secrets.txt
+echo "Nuclei secrets has finished on $domain -> $(wc -l < $vulns_path/secrets.txt)" | notify -bulk -silent
 
-# Subdomain takeover (Nuclei)
-echo "Subdomain takeover with nuclei on $domain" | notify -bulk -silent
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | nuclei -t takeovers -H "X-Forwarded-For: 127.0.0.1" -rl 40 -c 10 -o /root/results/$domain/vulns/takeovers.txt
-echo "Takeovers -> $(wc -l < /root/results/$domain/vulns/takeovers.txt)" | notify -bulk -silent
+# Fast lfi test
+echo "Listing LFI on $domain with nuclei" | notify -bulk -silent
+cat $wayback_data_path/*.txt $param_spider_path/*.txt | gf lfi | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | uro | nuclei -tags lfi -rl 40 -c 20 -p $proxy -o $vulns_path/LFI.txt
+echo "Nuclei LFI has finished on $domain -> $(wc -l < $vulns_path/LFI.txt) results" | notify -bulk -silent
 
-# Lfi -> (Test with burpsuite)
-
+# Fuzzing
+echo "Running dirsearch on $domain" | notify -bulk -silent
+dirsearch -w $wordlist_dir -exclude 404,403,401,400 -l $alive_subdomains_path --proxy $proxy --crawl -o /root/results/$domain/fuzzing/dirsearch.txt
+echo "Dirsearch has finished on $domain" | notify -bulk -silent
 
 # Port scan
-echo -e "Scanning ports with naabu on $domain" | notify -bulk -silent
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | dnsx -a -ro | naabu -silent -top-ports 1000 -exclude-ports 80,443,21,22,25 -o /root/results/$domain/ports/naabu_ports.txt
-echo -e "Naabu has finished on $domain -> $(wc -l < /root/results/$domain/ports/naabu_ports.txt)" | notify -bulk -silent
-
-# Nuclei tech detect
-echo -e "Running Nuclei for tech detect on $domain" | notify -bulk -silent
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | nuclei -t /root/tools/tech-detect.yaml -rl 40 -c 20 -o /root/results/$domain/technologies/nuclei_scan.txt
-
-# Dirsearch
-echo -e "Running dirsearch on $domain" | notify -bulk -silent
-dirsearch -w /root/tools/ElKraken/Tools/custom_wordlist.txt -l /root/results/$domain/httpx_info/alive_subdomains.txt --proxy=$proxy -t 40 -exclude 403,401,404,400 -o /root/results/$domain/fuzzing/dirsearch.txt
-echo -e "Dirsearch has finished on $domain -> $(wc -l < /root/results/$domain/fuzzing/dirsearch.txt) results" | notify -bulk silent
-
-# Corsy
-echo "Running Corsy on $domain" | notify -bulk -silent
-python3 /root/tools/Corsy/corsy.py -i /root/results/$domain/httpx_info/alive_subdomains.txt -o /root/results/$domain/vulns/cors.json
-
-# CRLFUZZ 
-echo "Running crlfuzz on $domain" | notify -bulk -silent
-crlfuzz -l /root/results/$domain/httpx_info/alive_subdomains.txt -o /root/results/$domain/vulns/crlf.txt
-
-# Screenshots
-echo "Taking screenshots on $domain" | notify -bulk -silent
-cat /root/results/$domain/httpx_info/alive_subdomains.txt | aquatone -chrome-path /snap/bin/chromium -out /root/results/$domain/aquatone
-echo "The scan has finished on $domain"
-
-if [ "$(tail -n 1 /root/tools/domains.txt)" == "$domain" ] ; then 
-  cd /root/tools/ElKraken/Tools/doxycannon
-  python3 doxycannon.py --down
-  sleep 1
-  docker rm $(docker ps -a -q)
-  docker rmi $(docker images -q)
-else
-  echo ""
-fi
+cat $alive_subdomains_path | naabu -top-ports 1000 -exclude-ports 80,443,21,22,25 -o /root/results/$domain/ports/naabu_ports.txt
