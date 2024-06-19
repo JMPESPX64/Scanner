1d57257560c90d17fc531d5dd0622df98dfac1f4
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-06-19 21:11:52 +0200

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -38,45 +38,5 @@ waymore -i $domain -mode U -oU $wayback_data_path/waymore.txt
 cat $alive_subdomains_path | katana -jc -d 5 -silent | tee -a $wayback_data_path/katana.txt
 paramspider -l $alive_subdomains_path
 
-# XSS
-echo "Running kxss on $domain" | notify -bulk -silent
-cat $wayback_data_path/*.txt | gf xss | uro | kxss | grep -v "\[\]" | tee -a $vulns_path/XSS.txt
-cat $param_spider_path/*.txt | kxss | grep -v "\[\]" | anew $vulns_path/XSS.txt
-echo "Kxss has finished -> $(cat $vulns_path/XSS.txt | grep '<' | wc -l) posible XSS" | notify -bulk -silent
+# ----------------------------------------------------------------- Vulnerabilities ------------------------------------------------------------------------------------
 
-# XSS (Freq)
-echo "Running freq on $domain" | notify -bulk -silent
-#cat $wayback_data_path/*.txt $param_spider_path/*.txt | grep "=" | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew $vulns_path/freq.txt | notify -bulk -silent
-proxychains4 -q bash -c "echo Y2F0ICR3YXliYWNrX2RhdGFfcGF0aC8qLnR4dCAkcGFyYW1fc3BpZGVyX3BhdGgvKi50eHQgfCBncmVwICI9IiB8IGVncmVwIC1pdiAiLihqcGd8anBlZ3xnaWZ8Y3NzfHRpZnx0aWZmfHBuZ3x0dGZ8d29mZnx3b2ZmMnxpY29ufHBkZnxzdmd8dHh0fGpzKSIgfCB1cm8gfCBxc3JlcGxhY2UgJyI+PGltZyBzcmM9eCBvbmVycm9yPWFsZXJ0KDEpOz4nIHwgZnJlcSB8IGVncmVwIC12ICdOb3QnIHwgYW5ldyAkdnVsbnNfcGF0aC9mcmVxLnR4dCB8IG5vdGlmeSAtYnVsayAtc2lsZW50Cg== | base64 -d | bash"
-echo "Freq has finished on $domain"
-
-# Secrets
-echo -e "Runnig nuclei for list secrets on $domain" | notify -bulk -silent
-cat $wayback_data_path/*.txt | grep "\.js$" | httpx -silent -mc 200 | tee -a $wayback_data_path/js.txt
-nuclei -l $wayback_data_path/js.txt -rl 40 -c 20 -t exposures -o $vulns_path/secrets.txt
-echo "Nuclei secrets has finished on $domain -> $(wc -l < $vulns_path/secrets.txt)" | notify -bulk -silent
-
-# Fast lfi test
-echo "Listing LFI on $domain with nuclei" | notify -bulk -silent
-cat $wayback_data_path/*.txt $param_spider_path/*.txt | gf lfi | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | uro | nuclei -tags lfi -rl 40 -c 20 -p $proxy -o $vulns_path/LFI.txt
-echo "Nuclei LFI has finished on $domain -> $(wc -l < $vulns_path/LFI.txt) results" | notify -bulk -silent
-
-# Takevoers
-echo "Running Nuclei for subdomain takeover on $domain" | notify -bulk -silent
-cat $alive_subdomains_path | nuclei -t takeovers -rl 40 -c 30 -o $vulns_path/takeovers.txt
-echo "Subdomain takeovers number -> $(wc -l < $vulns_path/takeovers.txt)" | notify -bulk -silent
-
-# Fuzzing
-echo "Running dirsearch on $domain" | notify -bulk -silent
-dirsearch -w $wordlist_dir -exclude 404,403,401,400 -l $alive_subdomains_path --proxy $proxy --crawl -o /root/results/$domain/fuzzing/dirsearch.txt
-echo "Dirsearch has finished on $domain" | notify -bulk -silent
-
-# Port scan
-echo "Scanning ports on $domain" | notify -bulk -silent
-cat $alive_subdomains_path | dnsx -a -ro | naabu -top-ports 1000 -exclude-ports 80,443,21,22,25 -silent | nuclei -t cves -rl 40 -c 20 -p $proxy -o $vulns_path/nuclei_ports_scan_cves.txt
-echo "The nuclei scan has finished on $domain -> $(wc -l < /root/results/$domain/vulns/nuclei_ports_scan_cves.txt)" | notify -bulk -silent
-
-# Screeshots
-echo "Taking screeshots on $domain" | notify -bulk -silent
-cat $alive_subdomains_path | aquatone -chrome-path /snap/bin/chromium -o /root/results/$domain/aquatone
-echo "Aquatone has finished on $domain" | notify -bulk -silent
