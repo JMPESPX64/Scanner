cbf5613226a74375aa22d80b9a586b66288c0e08
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-06-21 18:29:06 +0200

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -39,9 +39,16 @@ cat $alive_subdomains_path | katana -jc -d 5 -silent | tee -a $wayback_data_path
 paramspider -l $alive_subdomains_path
 
 # ----------------------------------------------------------------- Vulnerabilities ------------------------------------------------------------------------------------
-echo "Listing XSS on $domain" | notify -bulk -silent
+echo "Listing XSS on $domain with Kxss" | notify -bulk -silent
 cat $wayback_data_path/*.txt $param_spider_path/*.txt | gf xss | uro | kxss | grep -v "\[\]" | tee -a $vulns_path/XSS.txt
 echo "Kxss has finished on $domain" | notify -bulk -sileent
 
 # Fuzzing templates
+echo "Runnig fuzzing templates with nuclei on $domain" | notify -bulk -silent
+cat $wayback_data_path/*.txt $param_spider_path/*.txt | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | grep "=" | uro | nuclei -t /root/tools/fuzzing-templates -p $proxy -rl 40 -c 30 -dast -o $vulns_path/nuclei_fuzzing_templates
+echo "Nuclei fuzzing templates has finished on $domain -> $(wc -l < $vulns_path/nuclei_fuzzing_templates) results" | notify -bulk -silent
 
+# Secrets
+echo "Listing secrets with nuclei on $domain" | notify -bulk -silent
+cat $wayback_data_path/*.txt $param_spider_path/*.txt | grep "\.js$" | sort -u | httpx -silent | tee -a $wayback_data_path/js.txt
+nuclei -l $wayback_data_path/js.txt -t exposures -o $vulns_path/secrets.txt
